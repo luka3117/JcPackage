fd0a23655e19369125a2845443c4b2d0762d1d64
Jongchan Lee <jclee@JongchannoMacBook-Air.local>
2020-04-21 23:05:56 +0900

    2020年4月21日

--- OsakaUniv2020/R/hello.R
+++ OsakaUniv2020/R/hello.R
@@ -4,6 +4,21 @@ count.to.case <- function(df, count.variabe) {
   temp[-which(colnames(temp) == count.variabe)]
 }
 
-# count.to.case(df = df.count, count.variabe = "Freq")
+# count.to.case(df = df.count,  count.variabe = "Freq")
+
+Satisfaction <-
+  as.table(array(c(1, 2, 0, 0, 3, 3, 1, 2,
+                   11, 17, 8, 4, 2, 3, 5, 2,
+                   1, 0, 0, 0, 1, 3, 0, 1,
+                   2, 5, 7, 9, 1, 1, 3, 6),
+                 dim = c(4, 4, 2),
+                 dimnames =
+                   list(Income =
+                          c("<5000", "5000-15000",
+                            "15000-25000", ">25000"),
+                        "Job Satisfaction" =
+                          c("V_D", "L_S", "M_S", "V_S"),
+                        Gender = c("Female", "Male"))))
+
 
 
