8bd585d00031aec6dca059d0ff13a9fc1e5cee38
Jongchan Lee <jclee@JongchannoMacBook-Air.local>
2020-04-23 00:56:50 +0900

    2020年4月23日

---
+++ DoshishaUniv2020/test.R
@@ -0,0 +1,8 @@
+devtools::install_local("/Users/jclee/Dropbox/00000講義/同志社大学/DS Doshisha 2020 pkg/DD2020spring"
+                        ,force = T)
+
+devtools::install_github("luka3117/DD2020spring")
+
+
+
+
